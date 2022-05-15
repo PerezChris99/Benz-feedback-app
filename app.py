5d0659b3e76c6e7cb7fe0e3055782330962e4f3d
perezchris3599 <kweeziperez712@gmail.com>
2022-05-15 19:15:23 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        #print(customer, dealer, rating, comments)
+        if customer == '' or dealer == '':
+            return render_template('index.html', message='Please enter required fields')
+        return render_template('success.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
